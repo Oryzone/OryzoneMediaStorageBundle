e600b76b4aacf517971be016af9ea48a3298c83b
Luciano Mammino <lmammino@oryzone.com>
2012-02-26 21:54:16 +0100

    Fixed and commented IMedia interface

--- Entity/IMedia.php
+++ Entity/IMedia.php
@@ -2,9 +2,26 @@
 
 namespace Oryzone\Bundle\MediaStorageBundle\Entity;
 
-public interface IMedia
+/**
+ * Interface that must be implemented by all the entities that refers to one media
+ */
+interface IMedia
 {
+    /**
+     * @abstract
+     * @return int|string get the id of the entity
+     */
 	public function getId();
+
+    /**
+     * @abstract
+     * @return string get the name of the connected media
+     */
 	public function getMediaName();
+
+    /**
+     * @abstract
+     * @return string get the name of the media type
+     */
 	public function getMediaType();
 }
