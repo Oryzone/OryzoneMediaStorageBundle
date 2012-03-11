de9b2fed10c440ccf386eb55a861e7a486c489b6
Luciano Mammino <lmammino@oryzone.com>
2012-03-11 18:30:35 +0100

    Renamed MediaStorageAware to IMediaStorageAware to follow conventions
    Created MediaStorageAware abstract class

--- DependencyInjection/IMediaStorageAware.php
+++ DependencyInjection/IMediaStorageAware.php
@@ -8,20 +8,12 @@ use Oryzone\Bundle\MediaStorageBundle\Service\IMediaStorage;
  * Interface that should be implemented by all the classes that need to know whats the current media storage instance
  * (Injection of the current media storage is up to the developers)
  */
-abstract class MediaStorageAware implements IMediaStorageAware
+interface IMediaStorageAware
 {
     /**
-     * @var IMediaStorage the current media storage instance
-     */
-    protected $mediaStorage;
-
-    /**
      * Inject the current media storage
-     * @param IMediaStorage $mediaStorage
+     * @abstract
+     * @param \Oryzone\Bundle\MediaStorageBundle\Service\IMediaStorage $mediaStorage
      */
-    public function setMediaStorage(IMediaStorage $mediaStorage)
-    {
-        $this->mediaStorage = $mediaStorage;
-    }
-
+    public function setMediaStorage(IMediaStorage $mediaStorage);
 }
