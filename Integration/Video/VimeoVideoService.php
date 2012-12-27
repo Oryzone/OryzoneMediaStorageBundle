5f404a7d8af884c1853b32b02e6111ce64d11c62
lmammino <lmammino@oryzone.com>
2012-12-27 02:44:40 +0100

    Added vimeo integration
    small improvements on youtube integration

--- Integration/Video/VimeoVideoService.php
+++ Integration/Video/VimeoVideoService.php
@@ -4,18 +4,49 @@ namespace Oryzone\Bundle\MediaStorageBundle\Integration\Video;
 
 use Oryzone\Bundle\MediaStorageBundle\Exception\Integration\ResourceNotFoundException;
 
-class YoutubeVideoService extends VideoService
+class VimeoVideoService extends VideoService
 {
     /**
      * Url scheme for the API requests
      */
-    const API_URL = 'http://gdata.youtube.com/feeds/api/videos/%s';
+    const API_URL = 'http://vimeo.com/api/v2/video/%s.xml';
 
     /**
-     * Url scheme for the video preview image
-     * @const string PREVIEW_IMAGE_URL
+     * Scheme for xpath queries
      */
-    const PREVIEW_IMAGE_URL = 'http://img.youtube.com/vi/%s/0.jpg';
+    const XPATH_SCHEME = '/videos/video/%s';
+
+    /**
+     * The available metadata array
+     *
+     * @var array $AVAILABLE_METADATA
+     */
+    protected static $AVAILABLE_METADATA = array(
+        'id',
+        'title',
+        'description',
+        'url',
+        'upload_date',
+        'mobile_url',
+        'thumbnail_small',
+        'thumbnail_medium',
+        'thumbnail_large',
+        'user_id',
+        'user_name',
+        'user_url',
+        'user_portrait_small',
+        'user_portrait_medium',
+        'user_portrait_large',
+        'user_portrait_huge',
+        'stats_number_of_likes',
+        'stats_number_of_plays',
+        'stats_number_of_comments',
+        'duration',
+        'width',
+        'height',
+        'tags',
+        'embed_privacy',
+    );
 
     /**
      * Holds the DOMDocument used for XML parsing
@@ -32,30 +63,11 @@ class YoutubeVideoService extends VideoService
     protected $xpath;
 
     /**
-     * Xpaths used to extract data from response
-     * @var array $XPATHS
-     */
-    protected static $XPATHS = array(
-        'id'            =>  '/a:entry/a:id',
-        'published'     =>  '/a:entry/a:published',
-        'updated'       =>  '/a:entry/a:updated',
-        'title'         =>  '/a:entry/a:title',
-        'content'       =>  '/a:entry/a:content',
-        'tags'          =>  '(/a:entry/a:category/@term)[position()>1]',
-    );
-
-    /**
      * {@inheritDoc}
      */
     protected function getAvailableMetadata()
     {
-        return array(
-            'id',
-            'published',
-            'updated',
-            'title',
-            'content',
-        );
+        return self::$AVAILABLE_METADATA;
     }
 
     /**
@@ -63,21 +75,22 @@ class YoutubeVideoService extends VideoService
      */
     protected function loadMetaValue($name, $default = NULL)
     {
-        if(!isset(self::$XPATHS[$name]))
+        if(!in_array($name, self::$AVAILABLE_METADATA))
             return $default;
 
-        $elements = $this->xpath->query(self::$XPATHS[$name]);
+        $query = sprintf(self::XPATH_SCHEME, $name);
+        $elements = $this->xpath->query($query);
 
         switch($name)
         {
+            case 'description':
+                if (!is_null($elements) && $elements->length > 0)
+                    return strip_tags($elements->item(0)->nodeValue);
+                break;
+
             case 'tags':
                 if(!is_null($elements) && $elements->length > 0)
-                {
-                    $tags = array();
-                    foreach($elements as $element)
-                        $tags[] = $element->nodeValue;
-                    return $tags;
-                }
+                    return explode(", ", $elements->item(0)->nodeValue);
                 break;
 
             default:
@@ -103,14 +116,11 @@ class YoutubeVideoService extends VideoService
         if($response->isClientError() || $response->isServerError())
             throw new ResourceNotFoundException(sprintf('Cannot find youtube video with id "%s"', $id), $id);
 
-        $this->metadata['thumbnail'] = sprintf(self::PREVIEW_IMAGE_URL, $id);
-
         $rawResponse = $response->getContent();
 
         $this->document = new \DOMDocument();
         $this->document->loadXML($rawResponse);
         $this->xpath = new \DOMXpath($this->document);
-        $this->xpath->registerNamespace('a', 'http://www.w3.org/2005/Atom');
 
         return $rawResponse;
     }
