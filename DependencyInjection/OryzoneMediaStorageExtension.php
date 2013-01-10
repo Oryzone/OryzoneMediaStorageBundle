bd7f657dbcb41c846f6827e51e1a6fff989852c0
lmammino <lmammino@oryzone.com>
2013-01-10 20:17:25 +0100

    Created doctrine persistence adapter and modified bundle configuration accordigly

--- DependencyInjection/OryzoneMediaStorageExtension.php
+++ DependencyInjection/OryzoneMediaStorageExtension.php
@@ -2,10 +2,11 @@
 
 namespace Oryzone\Bundle\MediaStorageBundle\DependencyInjection;
 
-use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\Config\FileLocator;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
-use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\DependencyInjection\ContainerBuilder,
+    Symfony\Component\Config\FileLocator,
+    Symfony\Component\HttpKernel\DependencyInjection\Extension,
+    Symfony\Component\DependencyInjection\Loader,
+    Symfony\Component\DependencyInjection\Alias;
 
 /**
  * This is the class that loads and manages your bundle configuration
@@ -15,9 +16,9 @@ use Symfony\Component\DependencyInjection\Loader;
 class OryzoneMediaStorageExtension extends Extension
 {
 
-    protected $adapterMap = array(
-        'orm'     => 'Oryzone\Bundle\MediaStorageBundle\Listener\Adapter\ORM\DoctrineORMAdapter',
-        'mongodb' => 'Oryzone\Bundle\MediaStorageBundle\Listener\Adapter\ODM\MongoDB\MongoDBAdapter'
+    protected $objectManagerMap = array(
+        'orm'     => 'doctrine.orm.entity_manager',
+        'mongodb' => 'doctrine.odm.mongodb.document_manager'
     );
 
     /**
@@ -28,15 +29,15 @@ class OryzoneMediaStorageExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        $toLoad = array('cdns.xml', 'naming_strategies.xml', 'providers.xml', 'media_storage.xml', 'orm.xml',
-                        'templating.xml', 'form.xml', 'integration.xml', 'event_dispatcher_adapters.xml');
+        $toLoad = array('cdns.xml', 'naming_strategies.xml', 'providers.xml', 'media_storage.xml', 'templating.xml',
+            'form.xml', 'integration.xml', 'event_dispatcher_adapters.xml', 'persistence_adapters.xml');
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         foreach($toLoad as $file)
             $loader->load($file);
 
         // sets parameters from global config
         $dbDriver = $config['db_driver'];
-        $container->setAlias('oryzone_media_storage.listener.doctrine.adapter', 'oryzone_media_storage.listener.doctrine.adapter.'.$dbDriver);
+        $container->setAlias('oryzone_media_storage.persistence.doctrine.object_manager', new Alias($this->objectManagerMap[$dbDriver]));
 
         $container->setParameter('oryzone_media_storage.cdn_factory.cdns', $config['cdns']);
         $container->setParameter('oryzone_media_storage.context_factory.contexts', $config['contexts']);
---
+++ Persistence/DoctrinePersistenceAdapter.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Oryzone\Bundle\MediaStorageBundle\Persistence;
+
+use Doctrine\Common\Persistence\ObjectManager;
+
+use Oryzone\MediaStorage\Persistence\PersistenceAdapterInterface,
+    Oryzone\MediaStorage\Exception\PersistenceException,
+    Oryzone\MediaStorage\Model\MediaInterface;
+
+class DoctrinePersistenceAdapter implements PersistenceAdapterInterface
+{
+    /**
+     * @var \Doctrine\Common\Persistence\ObjectManager $objectManager
+     */
+    protected $objectManager;
+
+    /**
+     * @var bool $autoFlush
+     */
+    protected $autoFlush;
+
+    /**
+     * Constructor
+     *
+     * @param \Doctrine\Common\Persistence\ObjectManager $objectManager
+     * @param bool $autoFlush if <code>TRUE</code> will auto-flush the object manager after each change
+     */
+    public function __construct(ObjectManager $objectManager, $autoFlush = TRUE)
+    {
+        $this->objectManager = $objectManager;
+        $this->autoFlush = $autoFlush;
+    }
+
+    protected function callObjectManager(MediaInterface $media, $method = 'save')
+    {
+        try
+        {
+            if($method == 'remove')
+                $this->objectManager->remove($media);
+            else
+                $this->objectManager->persist($media);
+
+            if($this->autoFlush)
+                $this->objectManager->flush();
+        }
+        catch(\Exception $e)
+        {
+            throw new PersistenceException(sprintf('Cannot %s media "%s": %s', $method, $media->__toString(), $e->getMessage()), $this, 0, $e);
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function save(MediaInterface $media)
+    {
+        $this->callObjectManager($media);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function update(MediaInterface $media)
+    {
+        $this->callObjectManager($media, 'update');
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function remove(MediaInterface $media)
+    {
+        $this->callObjectManager($media, 'remove');
+    }
+}
